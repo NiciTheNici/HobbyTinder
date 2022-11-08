07f182c5ef26bd4e11dab1892ab276fdc8098b08
Muetzilla <muetz-github@gasbichler.ch>
2022-11-08 16:12:20 +0100

    init Spring Boot

---
+++ HobbyTinder/src/main/java/ch/bzz/hobbytinder/HobbyTinderApplication.java
@@ -0,0 +1,13 @@
+package ch.bzz.hobbytinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyTinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyTinderApplication.class, args);
+    }
+
+}
---
+++ HobbyTinder/src/test/java/ch/bzz/hobbytinder/HobbyTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bzz.hobbytinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyTinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
