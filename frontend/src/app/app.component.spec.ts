466fbb590b78464817ca76da56695fb8a02cfefd
NiciTheNici <nicolasvontobel@protonmail.ch>
2022-11-15 09:41:22 +0100

    recreated angular project without sass

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hobby_tinder'`, () => {
+  it(`should have as title 'Hobby_Tinder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hobby_tinder');
+    expect(app.title).toEqual('Hobby_Tinder');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hobby_tinder app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Hobby_Tinder app is running!');
   });
 });
